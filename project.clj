5c551b017decedbe7c76b3f5969e18cd30e47ee4
tonyvanriet <tonyvanriet@TvRsMBP.hsd1.il.comcast.net>
2014-11-21 21:37:32 -0600

    update project to clojure 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.5.7"]
                  [cheshire "4.0.4"]
                  [org.clojure/math.combinatorics "0.0.3"]]
